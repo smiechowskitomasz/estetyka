ee2fed1da1f0e0be17b2b22131be42cb33db8806
smiechowskitomasz <smiechowskitomasz@gmail.com>
2020-02-04 13:01:06 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const hamburgerBtn = document.querySelector("#hamburger");
-const showMenu = document.querySelector(".nav");
+const showMenu = document.querySelector(".nav__list");
+const addNavBcg = document.querySelector(".nav");
+let last_known_scroll_position = 0;
 
 hamburgerBtn.addEventListener("click", () => {
   showMenu.classList.toggle("activeMenu");
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 92) {
+    addNavBcg.classList.add("addNavBcg");
+  } else {
+    addNavBcg.classList.remove("addNavBcg");
+  }
 });
