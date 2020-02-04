b2d487cc42aca9d4c9342104e7a0c41ec60a45ed
smiechowskitomasz <smiechowskitomasz@gmail.com>
2020-02-04 16:02:01 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const hamburgerBtn = document.querySelector("#hamburger");
 const showMenu = document.querySelector(".nav__list");
-
+const aboutMeLink = document.getElementById("aboutMeLink");
+const contactLink = document.getElementById("contactLink");
 hamburgerBtn.addEventListener("click", () => {
   showMenu.classList.toggle("activeMenu");
+});
+aboutMeLink.addEventListener("click", () => {
+  showMenu.classList.remove("activeMenu");
+});
+
+contactLink.addEventListener("click", () => {
+  showMenu.classList.remove("activeMenu");
 });
